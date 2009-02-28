4b78bf52dc282ac2e2b15c25e39c57b47d1e581c
asteinborn78 <asteinborn78@2c15f996-05c1-11de-85a1-fd18384f7567>
2009-02-28 19:44:12 +0000

    Added random functions
    git-svn-id: https://sauos.googlecode.com/svn@10 2c15f996-05c1-11de-85a1-fd18384f7567

---
+++ trunk/kernel/random.c
@@ -0,0 +1,20 @@
+int seed_a = 32768;
+int seed_b = 65536;
+int tmp_rand;
+
+unsigned int rand(0
+{
+	tmp_rand = seed_a * seed_b * 3 - 2;
+	tmp_rand = (tmp_rand << seed_a) * seed_b * 5;
+	return tmp_rand;
+}
+
+int srand(int seed)
+{
+	if (seed == 1) return;
+	seed_a = seed;
+	seed_b = seed * 2;
+}
+
+unsigned int random() { return rand(); }
+int srand(int s) { return srand(s); }
