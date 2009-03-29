3756148365966fce54ad1d61c789d34d15c845d8
asteinborn78 <asteinborn78@2c15f996-05c1-11de-85a1-fd18384f7567>
2009-03-29 23:10:41 +0000

    git-svn-id: https://sauos.googlecode.com/svn@17 2c15f996-05c1-11de-85a1-fd18384f7567

--- trunk/fs/rwfs/fs.c
+++ trunk/fs/rwfs/fs.c
@@ -1,10 +1,1 @@
-/*
- * This file is part of the SauOS project.
- *
- * Copyright 2009 asteinborn78@gmail.com (Andrew Steinborn)
- *
- * Licensed under the GPLv2.
- */
-#include <sauos/fs.h>
-int rwfs_init(char *args) {}
-int rwfs_fini() {}
+
