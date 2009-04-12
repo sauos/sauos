c0aa31aaf39c805af5fa6cc328b56242ebe0daf1
asteinborn78 <asteinborn78@2c15f996-05c1-11de-85a1-fd18384f7567>
2009-04-12 21:55:35 +0000

    Remaking for new sturcture
    git-svn-id: https://sauos.googlecode.com/svn@30 2c15f996-05c1-11de-85a1-fd18384f7567

--- trunk/kernel/dl/dl_stubs.c
+++
@@ -1,4 +0,0 @@
-int _dl_error = 0;
-void *dlopen(const char *f, int fl){_dl_error = 1; return;}
-void *dlsym(void *h, const char *s){_dl_error = 2; return 0x80000000;}
-int dlclose(void *h){h = (void*)NULL; return;}
