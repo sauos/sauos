c49eb8ff0c1c745cb004e3eb2eed4d78e88a2bc7
bobbyjmoss <bobbyjmoss@2c15f996-05c1-11de-85a1-fd18384f7567>
2009-07-13 19:28:30 +0000

    Updated build script (taken directly from ScorchOS). I have temporarily omitted the lines to build the ATA driver so I can make a start on the many errors in the code! lol :D Note that you will be unable to run the build script until I upload the disk image (which is already setup with grub as a bootloader). Once that is done, you will also be able to run the test script.
    git-svn-id: https://sauos.googlecode.com/svn@56 2c15f996-05c1-11de-85a1-fd18384f7567

--- trunk/build.sh
+++ trunk/build.sh
@@ -1,20 +1,64 @@
 #!/bin/sh
-echo Now assembling, compiling, and linking your kernel:
-cd src
-nasm -f aout -o start.o start.asm
-gcc -Wall -Os -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o main.o main.c
-gcc -Wall -Os -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o scrn.o scrn.c
-gcc -Wall -Os -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o gdt.o gdt.c
-gcc -Wall -Os -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o idt.o idt.c
-gcc -Wall -Os -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o isrs.o isrs.c
-gcc -Wall -Os -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o irq.o irq.c
-gcc -Wall -Os -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o timer.o timer.c
-gcc -Wall -Os -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o kb.o kb.c
-gcc -Wall -Os -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o vsprintf.o vsprintf.c
-gcc -Wall -Os -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o ata_c.o ata_c.c
 
-nasm -f aout -o ata.o ata.s
-ld -T link.ld -o kernel.bin start.o main.o scrn.o gdt.o idt.o isrs.o irq.o timer.o kb.o ata.o ata_c.o vsprintf.o
-echo Cleaning up object files...
+# ScorchOS Build Script (Linux)
+# Copyright (C) 2008-9 ScorchOS Developers
+# For Terms see apl.txt and license.txt
+
+echo '> Starting Build...'
+
+# This makes sure the user is root (needed for the loop-back mount of the floppy
+# image I created earlier with a grub bootloader
+if test "`whoami`" != "root" ; then
+	echo "You must be logged in as root to build (for loopback mounting)"
+	echo "Enter 'su' or 'sudo bash' to switch to root"
+	exit
+fi
+
+cd src
+echo '> Assembling Bootloader...'
+# Compile bootloader
+nasm -f elf -o start.o start.asm
+
+echo '> Assembling ScorchOS Kernel Source...'
+#Compile Kernel Modules (C Programming Language)
+gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -fleading-underscore -I./include -o kernel.o -c kernel.c
+gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -fleading-underscore -I./include -o scrn.o -c scrn.c
+gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -fleading-underscore -I./include -o gdt.o -c gdt.c
+gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -fleading-underscore -I./include -o idt.o -c idt.c
+gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -fleading-underscore -I./include -o isrs.o -c isrs.c
+gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -fleading-underscore -I./include -o irq.o -c irq.c
+gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -fleading-underscore -I./include -o timer.o -c timer.c
+gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -fleading-underscore -I./include -o kb.o -c kb.c
+
+echo '> Creating Kernel Binary...'
+# Links the kernel modules together into a single binary
+ld -T link.ld -o kernel.x start.o kernel.o scrn.o gdt.o idt.o isrs.o irq.o timer.o kb.o
+
+cd ..
+echo '> Adding Kernel and Apps to Floppy Image...'
+#Time for that loop-back mount!
+rm -rf tmp-loop
+
+mkdir tmp-loop && mount -o loop -t vfat disk_image/sau.img tmp-loop
+# Remove previous kernel.x before adding the new one...
+cd tmp-loop && rm -rf kernel.x && cd ..
+cp src/kernel.x tmp-loop/kernel.x
+
+# Move included program binaries onto the floppy image (uncomment to use)
+#cp app/*.bin tmp-loop
+echo '> Unmounting Loop-back Floppy...'
+# Unmount the loop-back floppy
+umount tmp-loop || exit
+rm -rf tmp-loop
+
+echo '> Quick Cleanup...'
+# A quick cleanup (comment this out while debugging)
+cd src
 rm *.o
-echo Done!
+rm *.x
+cd ..
+# Note that (unlike MikeOS) you are not expected to carry source code in the
+# app directory - just the binaries. You can add additional build and
+# cleanup routines if you'd prefer to include souce however
+
+echo '> Build Complete!'
