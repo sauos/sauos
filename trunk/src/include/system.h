75dc3d1687fedaf03595c3fe933bb5028d5fea5d
bobbyjmoss <bobbyjmoss@2c15f996-05c1-11de-85a1-fd18384f7567>
2009-07-13 19:23:30 +0000

    Added Default header file. It still has the ScorchOS license on but we will deal with licensing once the kernel works!
    git-svn-id: https://sauos.googlecode.com/svn@55 2c15f996-05c1-11de-85a1-fd18384f7567

---
+++ trunk/src/include/system.h
@@ -0,0 +1,72 @@
+/*
+
+   Copyright 2008-09 ScorchOS Developers
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+/* System Header File */
+
+#ifndef __SYSTEM_H
+#define __SYSTEM_H
+
+typedef int size_t;
+
+/* This defines what the stack looks like after an ISR was running */
+struct regs
+{
+    unsigned int gs, fs, es, ds;
+    unsigned int edi, esi, ebp, esp, ebx, edx, ecx, eax;
+    unsigned int int_no, err_code;
+    unsigned int eip, cs, eflags, useresp, ss;    
+};
+
+/* KERNEL.C */
+extern void *memcpy(void *dest, const void *src, size_t count);
+extern void *memset(void *dest, char val, size_t count);
+extern unsigned short *memsetw(unsigned short *dest, unsigned short val, size_t count);
+extern size_t strlen(const char *str);
+extern unsigned char inportb (unsigned short _port);
+extern void outportb (unsigned short _port, unsigned char _data);
+
+/* SCRN.C */
+extern void init_video(void);
+extern void puts(unsigned char *text);
+extern void putch(unsigned char c);
+extern void cls();
+extern void shell_csr_fix();
+
+/* GDT.C */
+extern void gdt_set_gate(int num, unsigned long base, unsigned long limit, unsigned char access, unsigned char gran);
+extern void gdt_install();
+
+/* IDT.C */
+extern void idt_set_gate(unsigned char num, unsigned long base, unsigned short sel, unsigned char flags);
+extern void idt_install();
+
+/* ISRS.C */
+extern void isrs_install();
+
+/* IRQ.C */
+extern void irq_install_handler(int irq, void (*handler)(struct regs *r));
+extern void irq_uninstall_handler(int irq);
+extern void irq_install();
+
+/* TIMER.C */
+extern void timer_wait(int ticks);
+extern void timer_install();
+
+/* KB.C */
+extern void keyboard_install();
+
+#endif
