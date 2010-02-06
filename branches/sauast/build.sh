ba393543eeee716fb4e0df6323e2510fc120d8e7
Andrew Steinborn <geekme@arandomblog.co.cc>
2010-02-05 19:08:57 -0500

    Convert SVN dirs to git-style trunk.

--- branches/sauast/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo ">Compiling Kernel."
-gcc -o mainKernel.o -nostdinc -fno-builtin -c mainKernel.c
-gcc -o vsprintf.o -nostdinc -fno-builtin -c vsprintf.c
-nasm -f aout bootLoader.asm
-ld -T linkScript.x -o sauos bootLoader.o mainKernel.o vsprintf.o
-rm *.o
-echo ">Done - Result Binary Is 'sauos'"
